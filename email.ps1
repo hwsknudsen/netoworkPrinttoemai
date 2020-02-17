fea7d1f0531c5a8a2565e3f0c3b07d98a0742cf6
hwsknudsen <hwsknudsen@users.noreply.github.com>
2020-02-17 14:35:45 -0400

    Create email.ps1

---
+++ email.ps1
@@ -0,0 +1,25 @@
+Param (
+        [Parameter(ValueFromPipeline=$true)]
+        [String]$user = "username",
+        [String]$jobname = "jobname",
+        [String]$file = "filename"
+    )
+
+
+$fromaddress = "noreply@viking.bm" 
+$toaddress =  $user+"@viking.bm" 
+$Subject = ("Print To Email Job: "+$jobname) 
+$body = "Print to EMail By: "+$user
+$attachment = $file 
+$smtpserver = "viking-bm.mail.protection.outlook.com " 
+
+$message = new-object System.Net.Mail.MailMessage 
+$message.From = $fromaddress 
+$message.To.Add($toaddress)
+$message.IsBodyHtml = $True 
+$message.Subject = $Subject 
+$attach = new-object Net.Mail.Attachment($attachment) 
+$message.Attachments.Add($attach) 
+$message.body = $body 
+$smtp = new-object Net.Mail.SmtpClient($smtpserver) 
+$smtp.Send($message)
