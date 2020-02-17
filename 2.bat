6cd7492da05341141c79a53310756a21e18f9794
hwsknudsen <hwsknudsen@users.noreply.github.com>
2020-02-17 15:04:25 -0400

    Update 2.bat

--- 2.bat
+++ 2.bat
@@ -1,13 +1,6 @@
-SET /A test=%RANDOM%
-"C:\Program Files\gs\gs9.50\bin\gswin64c.exe" -dBATCH -dSAFER -dNOPAUSE -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -dAutoRotatePages=/PageByPage -r600 -sOutputFile="C:\PDFE\%test%.pdf" -
-cd "C:\PDFSCRIPT"
-
-powershell.exe -noexit "& C:\PDFSCRIPT\email.ps1 -user %1 -jobname %2 -file C:\pdfe\%test%.pdf"
-
-
-del C:\PDFE\%test%.pdf
-
-
+"C:\Program Files\gs\gs9.50\bin\gswin64c.exe" -dBATCH -dSAFER -dNOPAUSE -sDEVICE=pdfwrite -dPDFSETTINGS=/prepress -dAutoRotatePages=/PageByPage -r600 -sOutputFile="C:\PDFE\%2%.pdf" -
+powershell.exe -noexit "& C:\PDFSCRIPT\email.ps1 -user %1 -jobname %2 -file C:\pdfe\%2%.pdf"
+del C:\PDFE\%2%.pdf
 
 
 REM DO THIS.... Set-ExecutionPolicy -ExecutionPolicy Unrestricted
